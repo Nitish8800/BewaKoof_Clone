30c2470904560adb48054a74e263bd320ecf595b
Nitish Kumar <nitishfea120784@gmail.com>
2021-12-20 11:12:02 +0530

    I am Nitish Kumar

---
+++ store.js
@@ -0,0 +1,89 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready();
+}
+
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName('btn-danger');
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i];
+        button.addEventListener('click', removeCartItem);
+    }
+
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input');
+    for (var j = 0; j < quantityInputs.length; j++) {
+        var input = quantityInputs[j];
+        input.addEventListener('change', quantityChanged);
+    } 
+
+    var addToCartButtons = document.getElementsByClassName('shop-item-button');
+    for (var k = 0; k < addToCartButtons.length; k++) {
+        var buttn = addToCartButtons[k];
+        buttn.addEventListener('click', addToCartClicked);
+    }
+
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked);
+}
+
+function purchaseClicked() {
+    // alert('Thank you for your purchase');
+    var cartItems = document.getElementsByClassName('cart-items')[0];
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild);
+    }
+    updateCartTotal();
+}
+
+function removeCartItem(event) {
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal();
+}
+
+function quantityChanged(event) {
+    var input = event.target;
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1;
+    }
+    updateCartTotal();
+}
+
+function addToCartClicked(event) {
+    var button = event.target;
+    var shopItem = button.parentElement.parentElement;
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText;
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText;
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src;
+    addItemToCart(title, price, imageSrc);
+    updateCartTotal();
+}
+
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0];
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row');
+    var total = 0;
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i];
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0];
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0];
+        var price = parseFloat(priceElement.innerText.replace('₹ ', ''));
+        var quantity = quantityElement.value;
+        total = total + (price * quantity);
+    }
+    total = Math.round(total * 100) / 100;
+    document.getElementsByClassName('cart-total-price')[0].innerText = '₹ ' + total;
+}
+
+var count =0;
+
+var cartBtn = document.createElement("button");
+
+cartBtn.style.backgroundColor = "yellow";
+// // cartBtn.style.paddingBottom="20px";
+// cartBtn.style.marginTop="-40px";
+cartBtn.id="cartClassbtn";
+cartBtn.innerText="4";
+document.querySelector('#crt').append(cartBtn);
+
